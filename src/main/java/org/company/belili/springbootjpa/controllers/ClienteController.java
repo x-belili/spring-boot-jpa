d5c14ca8d8432f0eb620ada87809324080db022e
belili <bladimir.palomino@tecsup.edu.pe>
2019-02-22 05:18:26 -0500

    Loading image programmatically in the HTTP response
    Signed-off-by: belili <bladimir.palomino@tecsup.edu.pe>

--- src/main/java/org/company/belili/springbootjpa/controllers/ClienteController.java
+++ src/main/java/org/company/belili/springbootjpa/controllers/ClienteController.java
@@ -6,9 +6,13 @@ import org.company.belili.springbootjpa.util.paginator.PageRender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -19,6 +23,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -33,6 +38,28 @@ public class ClienteController {
     private IClienteService clienteService;
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @GetMapping(value = "/uploads/{filename:.+}")
+    public ResponseEntity<Resource> verFoto(@PathVariable String filename) {
+        Path pathFoto = Paths.get("uploads").resolve(filename).toAbsolutePath();
+        logger.info("pathFoto: " + pathFoto);
+        Resource resource = null;
+
+        try {
+            resource = new UrlResource(pathFoto.toUri());
+            if (!resource.exists() || !resource.isReadable()) {
+                throw new RuntimeException("Error no se puede cargar la imagen: " +
+                        pathFoto.toString());
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
 
     @GetMapping(value = {"/ver/{id}"})
     public String ver(@PathVariable(value = "id") Long id,
